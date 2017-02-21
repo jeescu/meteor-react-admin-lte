565f7fb9dd4e6c1735a6e2666103aa3f56417674
John Edward <john.escuyos@98labs.com>
2017-02-21 22:40:13 +0800

    fix less variable & mixin imports

--- client/main.js
+++ client/main.js
@@ -1,2 +1,8 @@
+// AdminLTE script
+import './lib/base/lib/admin-lte/dist/js/app.min.js';
+
+// Main HTML
 import './main.html';
+
+// Main JSX
 import '/imports/startup/client/index.jsx';
--- imports/ui/components/app/app_footer.jsx
+++ imports/ui/components/app/app_footer.jsx
@@ -6,7 +6,7 @@ export default class AppFooter extends Component {
 		return (
 			<footer className="main-footer">
 				<div className="pull-right hidden-xs">
-					<b>Version</b> 2.3.3
+					<b>Version</b> 2.3.11
 				</div>
 				<strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
 			</footer>
