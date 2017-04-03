41c433610a7a7a864000bd4549880d9d30b8c80b
John Edward <john.escuyos@98labs.com>
2017-04-03 18:25:42 +0800

    update unit test script

---
+++ test/server/index.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { chai } from 'meteor/practicalmeteor:chai';
+const { expect }  = chai;
+
+describe('Server test', function () {
+    it('Test case 1', function () {
+        expect(true).to.true;
+    });
+});
