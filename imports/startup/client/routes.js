be4fd35691dfd592864e1c17d949c34ef211e0e1
John Edward <john.escuyos@98labs.com>
2017-05-20 15:45:46 +0800

    route auth checking

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,23 +1,36 @@
+/* eslint-disable import/no-unresolved */
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import Index from '../../ui/components/index';
+import Home from '../../ui/components/home';
 import SignIn from '../../ui/components/sign_in';
 import SignUp from '../../ui/components/sign_up';
 import Dashboard from '../../ui/components/dashboard/dashboard';
 import Statistics from '../../ui/components/dashboard/views/statistics/statistics';
 import { NotFound } from '../../ui/pages/not_found/not_found';
 
+export const requireAuth = (nextState, replace) => {
+  // No user is authenticated redirect ro index
+  if (Meteor.user() === null) {
+    replace({
+      pathname: '/',
+    });
+  }
+};
+
 export const Routes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={Index}>
-      <Route path="/home" name="home" component={Index} />
-      <Route path="/sign-in" name="signIn" component={SignIn} />
-      <Route path="/sign-up" name="signUp" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard}>
-        <Route path="/dashboard" component={Statistics} />
+      <IndexRoute name="home" component={Home} />
+      <Route path="sign-in" name="signIn" component={SignIn} />
+      <Route path="sign-up" name="signUp" component={SignUp} />
+      <Route path="dashboard" name="dashboard" component={Dashboard} onEnter={requireAuth}>
+        <IndexRoute component={Statistics} />
+        <Route path="statistics" name="statistics" component={Statistics} />
       </Route>
     </Route>
-    <Route name="not-found" path="*" component={NotFound} />
+    <Route path="*" name="not-found" component={NotFound} />
   </Router>
 );
