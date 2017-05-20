e50d269fbe9a5f55d8c7c4a6da1298b46f584396
John Edward <john.escuyos@98labs.com>
2017-05-20 15:43:07 +0800

    added loading when page refresh

---
+++ imports/ui/layouts/loading/loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './loading.css';
+
+const Loading = () => (
+  <div className="text-center">
+    <i className="fa fa-spinner fa-pulse fa-3x fa-spin loading-spinner" />
+  </div>
+);
+
+export default Loading;
