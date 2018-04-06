6a6c5f9a87fe3ec60dfe6cd1beef609f36b241f5
John Edward <john.escuyos@98labs.com>
2018-04-06 17:12:27 +0800

    updated routing with custom route authentication

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,36 +1,24 @@
-/* eslint-disable import/no-unresolved */
-import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Switch } from 'react-router';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AuthRoute from './authRoute';
 
-import Index from '../../ui/components/index';
 import Home from '../../ui/components/home';
 import SignIn from '../../ui/components/sign_in';
 import SignUp from '../../ui/components/sign_up';
 import Dashboard from '../../ui/components/dashboard/dashboard';
-import Statistics from '../../ui/components/dashboard/views/statistics/statistics';
 import NotFound from '../../ui/pages/not_found/not_found';
 
-export const requireAuth = (nextState, replace) => {
-  // No user is authenticated redirect ro index
-  if (Meteor.user() === null) {
-    replace({
-      pathname: '/',
-    });
-  }
-};
-
-export const Routes = () => (
-  <Router history={browserHistory}>
-    <Route path="/" component={Index}>
-      <IndexRoute name="home" component={Home} />
-      <Route path="sign-in" name="signIn" component={SignIn} />
-      <Route path="sign-up" name="signUp" component={SignUp} />
-      <Route path="dashboard" name="dashboard" component={Dashboard} onEnter={requireAuth}>
-        <IndexRoute component={Statistics} />
-        <Route path="statistics" name="statistics" component={Statistics} />
-      </Route>
-    </Route>
-    <Route path="*" name="not-found" component={NotFound} />
+const Routes = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" name="home" component={Home} />
+      <Route path="/sign-in" name="signIn" component={SignIn} />
+      <Route path="/sign-up" name="signUp" component={SignUp} />
+      <AuthRoute path="/dashboard" name="dashboard" component={Dashboard} />
+      <Route name="not-found" component={NotFound} />
+    </Switch>
   </Router>
 );
+
+export default Routes;
