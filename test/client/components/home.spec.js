f832b5dcf60094937c6369b6ff9f1e6bda59a093
John Edward <john.escuyos@98labs.com>
2018-04-06 18:44:33 +0800

    updated client unit test

---
+++ test/client/components/home.spec.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import Adapter from 'enzyme-adapter-react-16';
+import Home from '../../../imports/ui/components/home';
+
+// TODO: centralized this config
+configure({ adapter: new Adapter() });
+
+describe('Home', () => {
+  it('should render home layout', () => {
+    const wrapper = shallow(<Home />);
+    console.log(wrapper.find('.lockscreen-wrapper').length);
+    expect(wrapper.find('.lockscreen-wrapper').length).to.equal(1);
+  });
+});
